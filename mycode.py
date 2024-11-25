f76a54903e4a100fb7ac46c39c8b39ace8f04a9e
Husam-AbuZina <husamzinap@gmail.com>
2024-11-26 00:25:47 +0200

    test

--- mycode.py
+++ mycode.py
@@ -1,17 +1,2 @@
-def add_positive(a, b):
-    if a > 0 and b > 0:
-        return a + b
-    else:
-        raise ValueError("Both numbers must be positivee")
-
-def add_zero(a, b):
-    if a == 0 or b == 0:
-        return a + b
-    else:
-        raise ValueError("At least one number must be zero")
-
-def add_negative(a, b):
-    if a < 0 and b < 0:
-        return a + b
-    else:
-        raise ValueError("Both numbers must be negative")
+def add_numbers(a, b):
+    return a+b
--- test_mycode.py
+++ test_mycode.py
@@ -1,14 +1,12 @@
 import pytest
-from mycode import add_positive, add_zero, add_negative
+
+from mycode import add_numbers
 
 def test_add_positive():
-    assert add_positive(3, 5) == 8
+    assert add_numbers(3, 5) == 8
 
 def test_add_zero():
-    assert add_zero(0, 5) == 5
+    assert add_numbers(0, 5) == 5
 
 def test_add_negative():
-    assert add_negative(-3, -5) == -8
-
-if __name__ == '__main__':
-    pytest.main()
+    assert add_numbers(-3, -5) == -8
